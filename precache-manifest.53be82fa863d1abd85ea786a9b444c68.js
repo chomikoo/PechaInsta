f6a4f7180f2534da1bd9e2a6fc1d947f545faed3
Szymon Trzepla <trzepla.szymon@gmail.com>
2018-12-16 14:59:11 +0100

    Updates

---
+++ precache-manifest.53be82fa863d1abd85ea786a9b444c68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be9b5584c299157091e9",
+    "url": "/PechaInsta/static/js/runtime~main.be9b5584.js"
+  },
+  {
+    "revision": "136ed8d1bb75c82590fe",
+    "url": "/PechaInsta/static/js/main.136ed8d1.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/PechaInsta/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "136ed8d1bb75c82590fe",
+    "url": "/PechaInsta/static/css/main.95a8fa11.chunk.css"
+  },
+  {
+    "revision": "d99d3f6ea5f2bdc72743cf29f57a94cb",
+    "url": "/PechaInsta/index.html"
+  }
+];
